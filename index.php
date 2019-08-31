2900457c6f1a9c7f6a3aec42584fa474aed86e59
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2019-08-31 08:01:55 +0600

    initilized project

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+use Prophecy\DDriver\SessionManager;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$test = new SessionManager();
+
+var_dump($test);
---
+++ src/SessionManager.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Prophecy\DDriver;
+
+class SessionManager {
+
+}

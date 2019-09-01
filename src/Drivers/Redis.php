94c6d6652e6ff58177d46d5bf41e8c754e95bb2a
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2019-09-01 09:41:51 +0600

    house keeping done for redis

--- src/Drivers/Redis.php
+++ src/Drivers/Redis.php
@@ -11,7 +11,13 @@ class Redis implements \SessionHandlerInterface {
      */
     private $redis;
 
-    public function __construct(string $host = '127.0.0.1', int $port = 6379 ,bool $enableTLS = false)
+    /**
+     * Redis constructor.
+     * @param string $host
+     * @param int $port
+     * @param bool $enableTLS
+     */
+    public function __construct(string $host = '127.0.0.1', int $port = 6379 , bool $enableTLS = false)
     {
         $conf = [
             'host'   => $host,
@@ -23,38 +29,57 @@ class Redis implements \SessionHandlerInterface {
 
         $this->redis = new Client($conf);
     }
-
-    public function close()
-    {
-        $this->redis->disconnect();
-
-        return true;
-    }
-
-    public function destroy($key)
-    {
-        return $this->redis->del((array)$key);
-    }
-
-    public function gc($maxlifetime)
-    {
-        return true;
-    }
-
+    /**
+     * @param string $save_path
+     * @param string $name
+     * @return bool
+     */
     public function open($save_path, $name)
     {
         $this->redis->connect();
 
         return true;
     }
-
+    /**
+     * @param string $session_id
+     * @param string $session_data
+     * @return bool|mixed
+     */
+    public function write($session_id, $session_data)
+    {
+        return $this->redis->set($session_id,$session_data);
+    }
+    /**
+     * @param string $session_id
+     * @return string
+     */
     public function read($session_id)
     {
         return $this->redis->get($session_id);
     }
-
-    public function write($session_id, $session_data)
+    /**
+     * @param string $key
+     * @return bool|int
+     */
+    public function destroy($key)
     {
-        return $this->redis->set($session_id,$session_data);
+        return $this->redis->del((array)$key);
+    }
+    /**
+     * @param int $maxlifetime
+     * @return bool
+     */
+    public function gc($maxlifetime)
+    {
+        return true;
+    }
+    /**
+     * @return bool
+     */
+    public function close()
+    {
+        $this->redis->disconnect();
+
+        return true;
     }
 }
